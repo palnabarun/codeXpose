e51b0805f8a90d704e2608dcbc9a9c999bb31474
Nabarun Pal <pal.nabarun95@gmail.com>
2018-07-07 14:40:21 +0530

    Refactors frontend config to get backend host from env

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,9 +1,10 @@
-export const login_api = "http://localhost:8000/interview/user/login/";
-export const question_get_api = "http://localhost:8000/interview/question/";
-export const test_get_api = "http://localhost:8000/interview/test/";
-export const create_user_api = "http://localhost:8000/interview/user/";
-export const schedule_test_api =
-  "http://localhost:8000/interview/schedule_test/";
-export const compile_api = "http://localhost:8000/interview/execute/run/";
-export const get_test_id_api =
-  "http://localhost:8000/interview/candidate/test/";
+const var host = process.env.BACKEND_HOST;
+const var api = "http://"+host+":8000";
+
+export const login_api = api+"/interview/user/login/";
+export const question_get_api = api+"/interview/question/";
+export const test_get_api = api+"/interview/test/";
+export const create_user_api = api+"/interview/user/";
+export const schedule_test_api = api+"/interview/schedule_test/";
+export const compile_api = api+"/interview/execute/run/";
+export const get_test_id_api = api+"/interview/candidate/test/";
